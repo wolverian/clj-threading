31ccb8721fb4cd0d73eddca0c8a3e3afb1b1b98d
Ilmari Vacklin <ilmari.vacklin@reaktor.fi>
2014-10-01 17:56:48 +0300

    Initial version

---
+++ main.rkt
@@ -0,0 +1,21 @@
+#lang racket/base
+
+(provide -> ->>)
+
+(define-syntax ->>
+  (syntax-rules ()
+    [(_ x)
+     x]
+    [(_ x (ss ...))
+     (ss ... x)]
+    [(_ x y z ...)
+     (->> (->> x y) z ...)]))
+
+(define-syntax ->
+  (syntax-rules ()
+    [(_ x)
+     x]
+    [(_ x (s ss ...))
+     (s x ss ...)]
+    [(_ x y z ...)
+     (-> (-> x y) z ...)]))
